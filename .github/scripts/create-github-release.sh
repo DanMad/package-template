18aff8c8c712b8b190b11fba8295da7cc061a2a3
Daniel Maddison <dmaddison@icloud.com>
2021-07-17 14:53:20 +1000

    Updated PR workflow

--- .github/scripts/create-github-release.sh
+++ .github/scripts/create-github-release.sh
@@ -1,12 +1,12 @@
 curl -v -s \
-  -H "Authorization: token ${GITHUB_TOKEN}" \
-  -H "Content-Type:application/json" \
-  "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases" \
-  -d "{ \
-    \"tag_name\": \"${{needs.get_package_version.outputs.git_tag_name}}\", \
-    \"target_commitish\": \"${GITHUB_SHA}\", \
-    \"name\": \"${{needs.get_package_version.outputs.git_tag_name}}\", \
-    \"body\": \"Release version ${{needs.get_package_version.outputs.git_tag_name}}\", \
-    \"draft\": false, \
-    \"prerelease\": false \
-  }"
+  -H 'Authorization: token ${GITHUB_TOKEN}' \
+  -H 'Content-Type:application/json' \
+  'https://api.github.com/repos/${GITHUB_REPOSITORY}/releases' \
+  -d '{ \
+    "tag_name": "${{ needs.get_package_version.outputs.git_tag_name }}", \
+    "target_commitish": "${ GITHUB_SHA }", \
+    "name": "${{ needs.get_package_version.outputs.git_tag_name }}", \
+    "body": "Release version ${{ needs.get_package_version.outputs.git_tag_name }}", \
+    "draft": false, \
+    "prerelease": false \
+  }'
